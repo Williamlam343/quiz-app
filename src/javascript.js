77f0bda85d68c7b59f0f354a27605f344ce4ccbc
Williamlam343 <87791763+Williamlam343@users.noreply.github.com>
2021-08-30 05:45:29 -0400

    lol this is so bad

--- src/javascript.js
+++ src/javascript.js
@@ -1,16 +1,54 @@
 // GIVEN I am taking a code quiz
 //TODO WHEN I click the start button
 
+
+
 var startbtn = document.querySelector("#startbtn")
 var timehtml = document.querySelector("#time")
 var scorehtml = document.querySelector("#score")
+var questionDisplay = document.querySelector("#question")
+var answerA = document.querySelector("#answer-a")
+var answerB = document.querySelector("#answer-b")
+var answerC = document.querySelector("#answer-c")
+var answerD = document.querySelector("#answer-d")
+// Used to display the question
+
 const quizquestions = [{
-    Question: "Hydrogen is an element?",
-    Answer: true,
+    question: "Hydrogen is an element?",
+    a: "True",
+    b: "False",
+    c: "Maybe",
+    d: "",
 }, {
-    Question: "Helium is a nobel gas?",
-    Answer: true,
+    question: "Lol is an element?",
+    a: "True",
+    b: "False",
+    c: "Maybe",
+    d: "",
+}, {
+    question: "Apple is an element?",
+    a: "True",
+    b: "False",
+    c: "Maybe",
+    d: "",
+}, {
+    question: "Chlorine is an element?",
+    a: "True",
+    b: "False",
+    c: "Maybe",
+    d: "",
+}, {
+    question: "Sodium is an element?",
+    a: "True",
+    b: "False",
+    c: "Maybe",
+    d: "",
+}, {
+    question: "Helium is a nobel gas?",
+    a: "True",
 }]
+
+let i = 0
 
 function quizApp() {
 
@@ -19,22 +57,36 @@ function quizApp() {
 
 
 
+
     // THEN a timer starts and I am presented with a question
     //counter that starts from 120 counts down; if user gets a question wrong subtract -5
     //timer starts when startbtn is clicked
     var timer = setInterval(() => {
         startTime--;
-        startbtn.setAttribute("style", "display:none")
+        // questionAnswers.textContent = `apple`
         timehtml.textContent = `${startTime} seconds remaining`
+        startbtn.setAttribute("style", "display:none")
+        scorehtml.textContent = `Score: ${score} `
 
-        scorehtml.textContent = `Score: ${score}`
+        questionDisplay.textContent = `Question #${i + 1}: ${quizquestions[i].question} `;
+        answerA.textContent = `A.${quizquestions[i].a} `;
+        answerB.textContent = `B.${quizquestions[i].b} `;
+        answerC.textContent = `C.${quizquestions[i].c} `;
+        answerD.textContent = `D.${quizquestions[i].d} `;
+
         if (startTime < 0) {
+
 
             clearInterval(timer);
             timehtml.textContent = "TIME'S UP";
             // timehtml.textContent = 120,
             startbtn.setAttribute("style", "display:block")
             startbtn.textContent = "Restart!"
+            questionDisplay.textContent = ``
+            answerA.textContent = ``;
+            answerB.textContent = ``;
+            answerC.textContent = ``;
+            answerD.textContent = ``;
         }
 
     }, 1000);
@@ -43,24 +95,29 @@ function quizApp() {
     //When start is pressed, a question is given in a display box
     //when answer is given display if correct or wrong; loop to next question
     //if question is wrong substract -5seconds from Timer
-    function questiondisplay() {
 
-    }
 
+
+
+
+
+
+
+    //TODO WHEN I answer a question
+    // THEN I am presented with another question
+    //TODO WHEN I answer a question incorrectly
+    // THEN time is subtracted from the clock
+    //TODO WHEN all questions are answered or the timer reaches 0
+    // THEN the game is over
+    //TODO WHEN the game is over
+    // THEN I can save my initials and my score
+    return ""
 }
-//TODO WHEN I answer a question
-// THEN I am presented with another question
-//TODO WHEN I answer a question incorrectly
-// THEN time is subtracted from the clock
-//TODO WHEN all questions are answered or the timer reaches 0
-// THEN the game is over
-//TODO WHEN the game is over
-// THEN I can save my initials and my score
 
 
-function start() {
-    console.log("uwu")
-}
 
 startbtn.addEventListener("click", quizApp)
+answerA.addEventListener("click", function nextquestion() {
+    i++; if (i > quizquestions.length) { i = 0 }
+})
 
