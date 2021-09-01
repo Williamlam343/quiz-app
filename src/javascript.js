13cf91024d71646f7e8c81663486da9a1bfb1e13
Williamlam343 <87791763+Williamlam343@users.noreply.github.com>
2021-08-31 23:41:16 -0400

    kinda works

--- src/javascript.js
+++ src/javascript.js
@@ -17,11 +17,11 @@ var hiScore = document.querySelector("#hiScore")
 
 // Used to display the question
 let n = 0;
-let startTime = 10
-let score = 0;
-let i = 0
+let i = 0;
 var highScoreName = []
 var highScoreNum = []
+let score = 0;
+let startTime = 60;
 const quizquestions = [{
     question: "Hydrogen is an element?",
     a: "True",
@@ -42,21 +42,21 @@ const quizquestions = [{
     b: "False",
     c: "Maybe",
     d: "????",
-    answer: "c",
+    answer: "a",
 }, {
     question: "Chlorine is an element?",
     a: "True",
     b: "False",
     c: "Maybe",
     d: "????",
-    answer: "d",
+    answer: "a",
 }, {
     question: "Sodium is an element?",
     a: "True",
     b: "False",
     c: "Maybe",
     d: "????",
-    answer: "d",
+    answer: "a",
 }, {
     question: "Helium is a nobel gas?",
     a: "True",
@@ -68,13 +68,17 @@ const quizquestions = [{
 
 // initializes the game
 
-
-// TODO
-function bgColorChanger() {
-
-
+function startGame() {
+    startTime = 60;
+    score = 0
+    i = 0
+    question.setAttribute("style", "visibility:visible")
+    answerA.setAttribute("style", "visibility:visible")
+    answerB.setAttribute("style", "visibility:visible")
+    answerC.setAttribute("style", "visibility:visible")
+    answerD.setAttribute("style", "visibility:visible")
+    quizApp()
 }
-
 
 function quizApp() {
     // THEN a timer starts and I am presented with a question
@@ -82,28 +86,22 @@ function quizApp() {
     //timer starts when startbtn is clicked
     startbtn.setAttribute("style", "visibility:hidden")
 
-
     var timer = setInterval(() => {
         startTime--;
         timehtml.textContent = `${startTime} seconds remaining`
 
 
-
-        if (startTime < 0) {
-
-
+        if (startTime <= 0) {
             clearInterval(timer);
+
             timehtml.textContent = "GAME_OVER";
-            // timehtml.textContent = 120,
-            // startbtn.setAttribute("style", "display:block")
-            // startbtn.textContent = "Restart!"
-            questionDisplay.textContent = "GAME OVER"
-            answerA.textContent = ``;
-            answerB.textContent = ``;
-            answerC.textContent = ``;
-            answerD.textContent = ``;
-
-
+            question.setAttribute("style", "visibility:hidden")
+            answerA.setAttribute("style", "visibility:hidden")
+            answerB.setAttribute("style", "visibility:hidden")
+            answerC.setAttribute("style", "visibility:hidden")
+            answerD.setAttribute("style", "visibility:hidden")
+            scoreInput.setAttribute("style", "visibility:visible")
+            // nextquestion.setAttribute("style", "visibility:hidden")
         }
 
     }, 1000);
@@ -111,41 +109,35 @@ function quizApp() {
     //When start is pressed, a question is given in a display box
     //when answer is given display if correct or wrong; loop to next question
     //if question is wrong substract -5seconds from Timer
-    nextquestion.addEventListener("click", function (event) {
-        var answerbox = event.target;
-        var dataAnswer = answerbox.getAttribute("data-answer")
+    if (startTime >= 0) {
+        nextquestion.addEventListener("click", function (event) {
+            var answerbox = event.target;
+            var dataAnswer = answerbox.getAttribute("data-answer")
 
-        if (quizquestions[i].answer === dataAnswer) {
-            score++;
-            scorehtml.textContent = `Score: ${score}`
-            alert("correct!")
-        } else {
-            alert(`wrong! ${startTime}s -5s`)
-            startTime -= 5;
+            if (quizquestions[i].answer === dataAnswer) {
+                score++;
+                scorehtml.textContent = `Score: ${score}`
 
-        }
+            } else {
 
-        if (answerbox.matches(".answer")) {
-            console.log(i)
-            i++;
-            if (i >= quizquestions.length) { startTime = 0 };
-            questionDisplay.textContent = `Question #${i + 1}: ${quizquestions[i].question} `;
-            answerA.textContent = `A. ${quizquestions[i].a} `;
-            answerB.textContent = `B. ${quizquestions[i].b} `;
-            answerC.textContent = `C. ${quizquestions[i].c} `;
-            answerD.textContent = `D. ${quizquestions[i].d} `;
+                startTime -= 5;
 
+            }
 
-        }
-    });
-    //TODO WHEN I answer a question
-    // THEN I am presented with another question
-    //TODO WHEN I answer a question incorrectly
-    // THEN time is subtracted from the clock
-    //TODO WHEN all questions are answered or the timer reaches 0
-    // THEN the game is over
-    //TODO WHEN the game is over
-    // THEN I can save my initials and my score
+            if (answerbox.matches(".answer")) {
+
+                i++;
+
+                if (i >= quizquestions.length) { i = 0; };
+                questionDisplay.textContent = `Question #${i + 1}: ${quizquestions[i].question} `;
+                answerA.textContent = `A. ${quizquestions[i].a} `;
+                answerB.textContent = `B. ${quizquestions[i].b} `;
+                answerC.textContent = `C. ${quizquestions[i].c} `;
+                answerD.textContent = `D. ${quizquestions[i].d} `;
+            }
+        })
+    }
+
     questionDisplay.textContent = `Question #${i + 1}: ${quizquestions[i].question} `;
     answerA.textContent = `A. ${quizquestions[i].a} `;
     answerB.textContent = `B. ${quizquestions[i].b} `;
@@ -158,14 +150,15 @@ function quizApp() {
 scoreRecorder.addEventListener("submit", function (event) {
     event.preventDefault()
     var scoreName = scoreInput.value
-
-
+    scoreInput.setAttribute("style", "visibility:hidden")
+    startbtn.setAttribute("style", "visibility:visible")
     if (scoreName === "") { return; }
     else {
-
         highScoreName.push(scoreName) || 0
         highScoreNum.push(score) || 0
         highScoreAdd();
+
+
     }
 
 
@@ -174,47 +167,48 @@ scoreRecorder.addEventListener("submit", function (event) {
 
 function highScoreAdd() {
 
-
-
-    let highNum = highScoreNum[n]
-    let highName = highScoreName[n]
-
+    var scoreHi = {
+        num: highScoreNum,
+        name: highScoreName,
+    }
+    scoreHi.num.sort
     var numP = document.createElement("p")
-    numP.textContent = highNum
-
     var nameP = document.createElement("p")
-    nameP.textContent = highName
+
+    numP.textContent = scoreHi.num[n]
+    nameP.textContent = scoreHi.name[n]
 
     hiScore.appendChild(numP)
     initials.appendChild(nameP)
     n++;
 
-    var hiScoreStorage = [{
-        name: highScoreName,
-        score: highScoreNum,
-    }];
-
-    localStorage.setItem("scores", JSON.stringify(hiScoreStorage))
-
+    localStorage.setItem("score", JSON.stringify(scoreHi))
 
 
 }
-var savedHighScore = JSON.parse(localStorage.getItem("scores"))
+
+
+
+
+
 function savedHiScore() {
-    for (let i = 0; i < savedHiScore.length; i++) {
+    scoreHi = JSON.parse(localStorage.getItem("score"))
 
+    for (let i = 0; i < scoreHi.name[0].length; i++) {
 
-        let highNum = savedHighScore[i].score[i]
-        let highName = savedHighScore[i].name[i]
+        scoreHi.num.sort((a, b) => b - a)
+
         var numP = document.createElement("p")
-        numP.textContent = highNum
         var nameP = document.createElement("p")
-        nameP.textContent = highName
+
+        numP.textContent = scoreHi.num[i]
+        nameP.textContent = scoreHi.name[i]
         hiScore.appendChild(numP)
         initials.appendChild(nameP)
+
     }
 }
 
 savedHiScore()
-startbtn.addEventListener("click", quizApp)
+startbtn.addEventListener("click", startGame)
 
